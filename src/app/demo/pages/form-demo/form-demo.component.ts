804fb1f54a4feafdefad77a993ff3869ace782fe
Sonal Wijenayaka <sonalwijenayaka@gmail.com>
2025-02-21 22:05:09 +0530

    .

--- src/app/demo/pages/form-demo/form-demo.component.ts
+++ src/app/demo/pages/form-demo/form-demo.component.ts
@@ -1,6 +1,29 @@
 import { Component } from '@angular/core';
 import { FormBuilder,FormGroup,FormControl } from '@angular/forms';
 import { FormDemoServiceService } from 'src/app/services/form-demo/form-demo-service.service';
+
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
+
 @Component({
   selector: 'app-form-demo',
   standalone: false,
@@ -11,6 +34,9 @@ export class FormDemoComponent {
              /* import form group ,form builder, form control */
       /* first_name, last_name age , email */
       demoForm: FormGroup;
+      displayedColumns: string[] = ['demo-position', 'demo-name', 'demo-weight', 'demo-symbol'];
+      dataSource = ELEMENT_DATA;
+
  constructor(private fb: FormBuilder,private demoService : FormDemoServiceService){
     this.demoForm = this.fb.group({
           firstName: new FormControl(''),
@@ -19,6 +45,8 @@ export class FormDemoComponent {
           email: new FormControl('')
     });
  }
+
+
 onSubmit(){
     console.log('form submitted');
     console.log(this.demoForm.value);
