cb48a86d81b9ebfbbc9ce97f154440758a539f37
Sonal Wijenayaka <sonalwijenayaka@gmail.com>
2025-03-11 15:23:13 +0530

    added validators
    added validators to form admin

--- src/app/demo/pages/form-demo/form-demo.component.ts
+++ src/app/demo/pages/form-demo/form-demo.component.ts
@@ -1,14 +1,17 @@
+import { MessageServiceService } from 'src/app/services/message-service/message-service.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder,FormGroup,FormControl } from '@angular/forms';
+import { FormBuilder,FormGroup,FormControl, Validators, AbstractControl } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { privateDecrypt } from 'crypto';
 import { FormDemoServiceService } from 'src/app/services/form-demo/form-demo-service.service';
 
 
 export interface PeriodicElement {
   name: string;
   position: number;
+
   weight: number;
   symbol: string;
 }
@@ -30,15 +33,22 @@ export class FormDemoComponent implements OnInit {
       mode = 'add';
       selectedData;
       isButtonDisabled: boolean = false;
+submitted: any;
 
 
 
- constructor(private fb: FormBuilder,private demoService : FormDemoServiceService){
+ constructor(
+  private fb: FormBuilder,
+  private demoService: FormDemoServiceService,
+  private MessageService: MessageServiceService
+
+  ){
+    // required, min ,max , minL ,pattern, customValidation
     this.demoForm = this.fb.group({
-          firstName: new FormControl(''),
-          lastName: new FormControl(''),
-          age: new FormControl(''),
-          email: new FormControl('')
+          firstName: new FormControl('' , Validators.required),
+          lastName: new FormControl('' ,  [Validators.required,Validators.minLength(3), Validators.maxLength(8)]),
+          age: new FormControl('', [Validators.required,Validators.min(14),Validators.max(120), this.customeAgeValidator]),
+          email: new FormControl('' , [Validators.required,Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ')]  ) //Validators.email
     });
  }
   ngOnInit(): void {
@@ -46,50 +56,128 @@ export class FormDemoComponent implements OnInit {
     this.populateData();
   }
 
+  customeAgeValidator(control: AbstractControl) {
+    if (!control) {
+      return null;
+    }
+    const controlValue = +control.value; // FIXED: Corrected variable name
+
+    if (isNaN(controlValue)) {
+      return { customAgeValidator: true };
+    }
+
+    return null;
+  }
+
+
+
+// public populateData(): void{
+//     //implemet get data code
+//     //ts -> service file function
+//     // service -> backend call
+//     this.demoService.getData().subscribe((response: any[]) => {
+//        console.log('get data response' , response);
+
+//        this.dataSource = new MatTableDataSource(response);
+//        this.dataSource.paginator = this.paginator;
+//        this.dataSource.sort =this.sort;
+//     });
+// }
+
 
 public populateData(): void{
-    //implemet get data code
-    //ts -> service file function
-    // service -> backend call
-    this.demoService.getData().subscribe((response: any[]) => {
-       console.log('get data response' , response);
+  //implemet get data code
+  //ts -> service file function
+  // service -> backend call
 
-       this.dataSource = new MatTableDataSource(response);
-       this.dataSource.paginator = this.paginator;
-       this.dataSource.sort =this.sort;
+  try{
+    this.demoService.getData().subscribe({
+       next: (dataList: any[]) => {
+         if (dataList.length <= 0){
+            return;
+         }
+        this.dataSource = new MatTableDataSource(dataList);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort =this.sort;
+       }, error:(error)=> {
+        this.MessageService.showError('Action failed with error' + error);
+       }
     });
+  }catch(error){
+    this.MessageService.showError('Action failed with error' + error);
+  }
 }
+
 onSubmit(){
-    console.log('Mode' + this.mode)
+  try{
+    console.log('Mode' + this.mode);
     console.log('form submitted');
     console.log(this.demoForm.value);
 
+    this.submitted = true;
     if(this.mode == 'add'){
-      this.demoService.serviceCall(this.demoForm.value).subscribe((response)=>{
+  //     this.demoService.serviceCall(this.demoForm.value).subscribe((response)=>{
 
-        if(this.dataSource && this.dataSource.data && this.dataSource.data.length>0){
-             this.dataSource = new MatTableDataSource([response, ...this.dataSource.data])
-        } else{
-          this.dataSource = new MatTableDataSource([response]); //input data goes to the top of the display
-        }
-   });
+  //       if(this.dataSource && this.dataSource.data && this.dataSource.data.length>0){
+  //            this.dataSource = new MatTableDataSource([response, ...this.dataSource.data])
+  //       } else{
+  //         this.dataSource = new MatTableDataSource([response]); //input data goes to the top of the display
+  //       }
+
+  //       this.MessageService.showSuccess('Data saved successfully!');
+  //  });
+  this.demoService.serviceCall(this.demoForm.value).subscribe({
+    next: (response: any) => {
+      if(this.dataSource && this.dataSource.data && this.dataSource.data.length>0){
+                   this.dataSource = new MatTableDataSource([response, ...this.dataSource.data])
+              } else{
+                this.dataSource = new MatTableDataSource([response]); //input data goes to the top of the display
+              }
+
+              this.MessageService.showSuccess('Data saved successfully!');
+         },
+         error: (error)=> {
+             this.MessageService.showError('Action failed with erro' + error);
+         }
+});
 
     } else if (this.mode === 'edit'){
-       this.demoService.editData(this.selectedData?.id, this.demoForm.value).subscribe((response) =>{
+      //  this.demoService.editData(this.selectedData?.id, this.demoForm.value).subscribe((response) =>{
+      //    let elementIndex = this.dataSource.data.findIndex((element) => element.id === this.selectedData?.id);
+      //    this.dataSource.data[elementIndex] = response;
+      //    this.dataSource = new MatTableDataSource(this.dataSource.data);
+      //    this.MessageService.showSuccess('Data  edited successfully!');
+
+      //  });
+
+      this.demoService.editData(this.selectedData?.id, this.demoForm.value).subscribe({
+          next: (response: any)=>{
          let elementIndex = this.dataSource.data.findIndex((element) => element.id === this.selectedData?.id);
          this.dataSource.data[elementIndex] = response;
          this.dataSource = new MatTableDataSource(this.dataSource.data);
-       });
+         this.MessageService.showSuccess('Data  edited successfully!');
+          },
+          error: (error)=> {
+            this.MessageService.showError('Action failed with erro' + error);
+        }
+      });
     }
       this.mode = 'add';
       this.demoForm.disable(); // automatically disable the form after submitting form data
       this.isButtonDisabled = true;
+  }catch(error){
+
+        this.MessageService.showError('Action failed with error' + error);
+  }
 }
 public resetData(): void {
   this.demoForm.reset();
-  this.saveButtonLabel = "Save";
+  this.demoForm.setErrors = null;
+  this.demoForm.updateValueAndValidity();
+  this.saveButtonLabel = 'save';
   this.demoForm.enable();
   this.isButtonDisabled = false;
+  this.submitted = false;
 }
 
 public editData(data:any): void {
@@ -105,14 +193,35 @@ public deleteData(data:any): void {
 
 const id = data.id;
 
-this.demoService.deleteData(id).subscribe((response) => {
-const index = this.dataSource.data.findIndex((element) => element.id == id);
-if(index !== -1){
-   this.dataSource.data.splice( index,1 );
-}
-this.dataSource = new MatTableDataSource(this.dataSource.data);
+try{
+  // this.demoService.deleteData(id).subscribe((response) => {
+  //   const index = this.dataSource.data.findIndex((element) => element.id == id);
+  //   if(index !== -1){
+  //      this.dataSource.data.splice( index,1 );
+  //   }
+  //   this.dataSource = new MatTableDataSource(this.dataSource.data);
+  //   this.MessageService.showSuccess('Data deleted successfully!');
+  //   });
 
-});
+  this.demoService.deleteData(id).subscribe({
+    next: (response: any)=>{
+      const index = this.dataSource.data.findIndex((element) => element.id == id);
+      if(index !== -1){
+         this.dataSource.data.splice( index,1 );
+      }
+      this.dataSource = new MatTableDataSource(this.dataSource.data);
+      this.MessageService.showSuccess('Data deleted successfully!');
+    },   error: (error)=> {
+      this.MessageService.showError('Action failed with erro' + error);
+  }
+    });
+
+
+
+
+}catch(error){
+
+  this.MessageService.showError('Action failed with error' + error);}
 }
 
 applyFilter(event: Event) {
@@ -125,8 +234,5 @@ applyFilter(event: Event) {
 
   public refreshData(): void {
      this.populateData();
-  }
-
-
-
+     }
 }
