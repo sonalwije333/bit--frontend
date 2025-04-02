cff78753641974c651144cd89f642b1da00ec517
Sonal Wijenayaka <sonalwijenayaka@gmail.com>
2025-04-02 11:49:23 +0530

    refactor: added validations to customer form

--- src/app/demo/pages/customer/customer.component.ts
+++ src/app/demo/pages/customer/customer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup,FormControl, Validators, AbstractControl } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-customer',
@@ -10,6 +11,14 @@ import { FormBuilder, FormGroup,FormControl, Validators, AbstractControl } from 
 export class CustomerComponent {
 
   customerForm: FormGroup;
+  customerService: any;
+  mode: string;
+  submitted: boolean;
+  dataSource: any;
+  MessageService: any;
+  demoService: any;
+  selectedData: any;
+  isButtonDisabled: boolean;
 
   constructor(private fb: FormBuilder){
     this.customerForm = this.fb.group({
@@ -24,9 +33,51 @@ export class CustomerComponent {
   }
 
   onSubmit(){
+    try{
+      console.log('Mode' + this.mode);
+      console.log('form submitted');
+      console.log(this.customerForm.value);
 
-    console.log('form submitted');
-    console.log(this.customerForm.value);
+      this.submitted = true;
+      if(this.mode == 'add'){
+
+    this.customerService.serviceCall(this.customerForm.value).subscribe({
+      next: (response: any) => {
+        if(this.dataSource && this.dataSource.data && this.dataSource.data.length>0){
+                     this.dataSource = new MatTableDataSource([response, ...this.dataSource.data])
+                } else{
+                  this.dataSource = new MatTableDataSource([response]); //input data goes to the top of the display
+                }
+
+                this.MessageService.showSuccess('Data saved successfully!');
+           },
+           error: (error)=> {
+               this.MessageService.showError('Action failed with erro' + error);
+           }
+  });
+
+      } else if (this.mode === 'edit'){
+
+
+        this.demoService.editData(this.selectedData?.id, this.customerForm.value).subscribe({
+            next: (response: any)=>{
+           let elementIndex = this.dataSource.data.findIndex((element) => element.id === this.selectedData?.id);
+           this.dataSource.data[elementIndex] = response;
+           this.dataSource = new MatTableDataSource(this.dataSource.data);
+           this.MessageService.showSuccess('Data  edited successfully!');
+            },
+            error: (error)=> {
+              this.MessageService.showError('Action failed with erro' + error);
+          }
+        });
+      }
+        this.mode = 'add';
+        this.customerForm.disable(); // automatically disable the form after submitting form data
+        this.isButtonDisabled = true;
+    }catch(error){
+
+          this.MessageService.showError('Action failed with error' + error);
+    }
   }
 
 }
