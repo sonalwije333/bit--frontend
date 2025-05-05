f18895f9edbf4d590a045faf745a2c9e2a41492e
Sonal Wijenayaka <sonalwijenayaka@gmail.com>
2025-05-05 16:41:46 +0530

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   production: true,
-  baseUrl: 'https://your-production-url.com',
+  baseUrl: 'http://localhost:8080',
   appVersion: '1.0.0', // Add this line
 };
