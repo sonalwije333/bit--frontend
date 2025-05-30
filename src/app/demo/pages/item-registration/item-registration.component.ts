11c4fd309291744b76bd2f16ff6e0899b413341c
Sonal Wijenayaka <sonalwijenayaka@gmail.com>
2025-05-30 15:26:14 +0530

    feat: item-registration component

---
+++ src/app/demo/pages/item-registration/item-registration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-registration',
+  imports: [],
+  templateUrl: './item-registration.component.html',
+  styleUrl: './item-registration.component.scss'
+})
+export class ItemRegistrationComponent {
+
+}
